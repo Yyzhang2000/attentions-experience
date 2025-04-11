223cc3d56d02958d2600483f57488e2184cd932a
YuyangZhang <yuyangzhang2025@gmail.com>
2025-04-12 01:36:27 +0800

    Add Metrics

---
+++ metrics.py
@@ -0,0 +1,21 @@
+import time
+
+import psutil
+
+
+def compute_tokens_per_second(batch_tokens, elapsed_time):
+    if elapsed_time == 0:
+        return 0.0
+    return batch_tokens / elapsed_time
+
+
+def compute_memory_usage():
+    process = psutil.Process()
+    memory_info = process.memory_info()
+    return memory_info.rss / (1024 * 1024)  # Convert bytes to MB
+
+
+METRICS_REGISTRY = {
+    "tokens_per_second": compute_tokens_per_second,
+    "memory_usage": compute_memory_usage,
+}
